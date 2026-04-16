15841857c291c0ae20d654dd2b883cfc80ef4805
tomaspiaggio <tomas.piaggio12@gmail.com>
2026-04-15 20:02:11 -0700

    feat: started layer operations (so far only linear)

--- include/layer_operations.h
+++ include/layer_operations.h
@@ -3,6 +3,7 @@
 #ifndef LAYER_OPERATIONS_H
 #define LAYER_OPERATIONS_H
 
-int linear_forward(Tensor* input, Tensor* weights, Tensor* bias, Tensor* outputs, Tensor* batch, Tensor* in_dim, Tensor* out_dim);
+int linear_forward(Tensor* input, Tensor* weights, Tensor* bias, Tensor* outputs);
+int linear_backward();
 
 #endif
--- src/layer_operations.c
+++ src/layer_operations.c
@@ -1,1 +1,21 @@
 #include "../include/layer_operations.h"
+#include "../include/tensor_math.h"
+
+int linear_forward(Tensor* input, Tensor* weights, Tensor* bias, Tensor* outputs) {
+    int result = matmul(input, weights, outputs);
+
+    if (result != 0) {
+        return result;
+    }
+
+    // in this case it's safe to reuse the output since the add goes from left to right so
+    // the value we're stepping over was already used. in this way, we don't have to create
+    // a new tensor each time we call a foward pass and we avoid the allcoation alltogether
+    result = elementwise_add(outputs, bias, outputs);
+
+    if (result != 0) {
+        return result;
+    }
+
+    return 0;
+}
